b6ab1b52c3c5e12a701efa04a50e8fbca75ed081
MCX <mc@mcx.ink>
2022-07-15 13:42:03 -0600

    GH Actions test on Stage workflow test.

--- web/app/app/settings.py
+++ web/app/app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # External - just wait a moment:
+    # External - just wait:
 ]
 
 MIDDLEWARE = [
