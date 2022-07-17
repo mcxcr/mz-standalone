243772130bb9019ba2c151932318b62fc61522a0
MCX <mc@mcx.ink>
2022-07-17 16:19:32 -0600

    Migrate script fixed.

--- web/app/migrate.sh
+++ web/app/migrate.sh
@@ -3,5 +3,5 @@
 SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"admin@mazorka.com"}
 cd /app/
 
-/opt/venv/bin/python manage.py migrate --noinput
-/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
+/opt/venvmz/bin/python manage.py migrate --noinput
+/opt/venvmz/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
