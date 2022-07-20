83b51023bb1b7d3938e95e2ee2a85f82d612a16b
MCX <mc@mcx.ink>
2022-07-19 21:34:15 -0600

    DO Space configured for staging.

--- web/app/core/tests/test_admin.py
+++ web/app/core/tests/test_admin.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.test import Client
 from django.contrib.auth import get_user_model
-from django.urls import reverse
+from django.urls import reverse # noqa
 
 
 class AdminSiteTests(TestCase):
